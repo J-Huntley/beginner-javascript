dac1eb38e155acfd64b96b0ed840441b6ffb7ec1
Joseph S. Huntley <huntley.27@osu.edu>
2023-07-11 15:33:22 -0500

    variables types continued

---
+++ playground/types.js
@@ -0,0 +1,11 @@
+// console.log('it works');
+// const name ='joe';
+// const middle = "sam";
+// const last = `hunt`;
+// const sentence = 'she\'s so cool \\';
+
+const person = {
+    first: "wes",
+    last: "bos",
+    age:100,
+};
