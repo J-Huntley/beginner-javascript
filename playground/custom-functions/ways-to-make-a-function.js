95bd25dc233f04bc4a3776698e17369d03a06b55
Joseph S. Huntley <huntley.27@osu.edu>
2023-08-08 14:52:12 -0500

    more function experimentation

--- playground/custom-functions/ways-to-make-a-function.js
+++ playground/custom-functions/ways-to-make-a-function.js
@@ -1,3 +1,4 @@
+/*
 console.log("it works");
 console.log(doctorize2('wes'));
 //function is hoisted, function expression would not
@@ -8,3 +9,15 @@ function doctorize2 (firstName) {
     return `Dr. ${firstName}`;
 }
 const inchToCM = (inches) => inches * 2.54;
+*/
+
+const wes = {
+    name: "Wes",
+    sayHi: function sayHi(){
+        console.log(`hey ${this.name}`);
+        return "Hey wes!";
+    }
+}
+
+const button = document.querySelector(".clickMe");
+console.log(button);
