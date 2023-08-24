403f4beafe19a2c2d7b8b06a2326cd9131f09d10
Joseph S. Huntley <huntley.27@osu.edu>
2023-08-24 15:06:22 -0500

    working with the DOM

---
+++ exercises/20 - The DOM/the-dom.js
@@ -0,0 +1,6 @@
+// const p = document.querySelector('p');
+// const divs = document.querySelectorAll('div');
+// console.log(p);
+// console.log(divs);
+const heading = document.querySelector('h2');
+console.dir(heading.textContent);
